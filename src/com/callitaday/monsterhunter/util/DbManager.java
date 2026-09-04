d51cc509fa480fd21ffdfa59d07117a302e1c0ce
imnotpossib1e <habit2me@gmail.com>
2026-09-04 23:13:59 +0900

    DbManager 작성

--- src/com/callitaday/monsterhunter/util/DbManager.java
+++ src/com/callitaday/monsterhunter/util/DbManager.java
@@ -1,5 +1,62 @@
 package com.callitaday.monsterhunter.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbManager {
+    // 고정값을 상수 필드로 관리 -> 인터페이스에 선언(인터페이스는 모두 상수)
+
+    /**
+     * 로드
+     */
+    static {
+        try {
+            Class.forName(DbProperties.DRIVER_NAME);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 연결
+     */
+    public static Connection getConnection() throws SQLException {
+        Connection con = DriverManager.getConnection(DbProperties.URL, DbProperties.USER_ID,
+                                                     DbProperties.USER_PASS);
+        return con;
+    }
+
+    /**
+     * 닫기 (DDl & DML인 경우)
+     */
+    public static void dbClose(Connection con, Statement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 닫기 (SELECT인 경우)
+     */
+    public static void dbClose(Connection con, Statement st, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            dbClose(con, st);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
