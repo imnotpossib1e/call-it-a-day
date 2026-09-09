ed9d05b67c667ce3897d39884b43eb2a4d9e5503
imnotpossib1e <habit2me@gmail.com>
2026-09-09 19:34:45 +0900

    무기/방어구 다량 보유 방지 로직 추가

--- src/com/callitaday/monsterhunter/dao/InventoryDaoImpl.java
+++ src/com/callitaday/monsterhunter/dao/InventoryDaoImpl.java
@@ -44,7 +44,7 @@ public class InventoryDaoImpl implements InventoryDao {
                 itemDto.setItemName(rs.getString("item_name"));
                 itemDto.setItemIncrease(rs.getInt("item_increase"));
                 itemDto.setItemExplanation(rs.getString("item_explanation"));
-                itemDto.setItemType(rs.getInt("item_type"));
+                itemDto.setItemType(rs.getString("item_type"));
                 inventoryDto.setItemDto(itemDto);
                 list.add(inventoryDto);
             }
@@ -54,5 +54,37 @@ public class InventoryDaoImpl implements InventoryDao {
         }
 
         return list;
+    }
+
+    /**
+     * 아이템 보유 수량 체크
+     *
+     * @param user_id
+     * @param item_id
+     */
+    @Override
+    public InventoryDto getItemQuantity(int user_id, int item_id) throws SQLException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String sql = "select * from inventory where user_id=? and item_id = ?";
+
+        InventoryDto inventoryDto = null;
+
+        try{
+            con= DbManager.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, user_id);
+            ps.setInt(2, item_id);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                inventoryDto = new InventoryDto(rs.getInt("user_id"), rs.getInt("quantity"), "T".equals(rs.getString("is_equipped")), rs.getInt("item_id"));
+            }
+        }finally {
+            DbManager.dbClose(con, ps, rs);
+        }
+        return inventoryDto;
     }
 }
