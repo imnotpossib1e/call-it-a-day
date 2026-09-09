62a1167444bddcda6016a166977b9da34270faed
imnotpossib1e <habit2me@gmail.com>
2026-09-09 20:56:20 +0900

    Item DAO 인벤토리 로직 분리

--- src/com/callitaday/monsterhunter/dao/ItemDaoImpl.java
+++ src/com/callitaday/monsterhunter/dao/ItemDaoImpl.java
@@ -17,49 +17,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDaoImpl implements ItemDao {
-    CharactorInfoDao charactorInfoDao = new CharactorInfoDaoImpl();
-
-    /**
-     * 내가 보유한 아이템 조회
-     *
-     * Select
-     *
-     * @param userId
-     */
-    @Override
-    public List<InventoryDto> getItemInfo(int userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        String sql = "select * from v_user_inventory where user_id = ?";
-        List<InventoryDto> list = new ArrayList<InventoryDto>();
-
-        try{
-            con = DbManager.getConnection();
-            ps = con.prepareStatement(sql);
-            ps.setInt(1, userId);
-            rs = ps.executeQuery();
-
-            while(rs.next()){
-                InventoryDto inventoryDto = new InventoryDto();
-                inventoryDto.setUserId(rs.getInt("user_id"));
-                inventoryDto.setQuantity(rs.getInt("quantity"));
-                inventoryDto.setEquipped("T".equals(rs.getString("is_equipped")));
-                ItemDto itemDto = new ItemDto();
-                itemDto.setItemName(rs.getString("item_name"));
-                itemDto.setItemIncrease(rs.getInt("item_increase"));
-                itemDto.setItemType(rs.getInt("item_type"));
-                inventoryDto.setItemDto(itemDto);
-                list.add(inventoryDto);
-            }
-        }
-        finally {
-            DbManager.dbClose(con, ps, rs);
-        }
-
-        return list;
-    }
 
     /**
      * 아이템 단일 조회
@@ -80,7 +37,7 @@ public class ItemDaoImpl implements ItemDao {
             ps.setInt(1, item_id);
             rs = ps.executeQuery();
             if(rs.next()){
-                itemDto = new ItemDto(rs.getInt("item_id"), rs.getString("item_name"), rs.getInt("item_price"), rs.getInt("item_increase"), rs.getInt("item_type"));
+                itemDto = new ItemDto(rs.getInt("item_id"), rs.getString("item_name"), rs.getInt("item_price"), rs.getInt("item_increase"), rs.getString("item_explanation"), rs.getString("item_type"));
             }
         }finally {
             DbManager.dbClose(con, ps, rs);
@@ -112,7 +69,8 @@ public class ItemDaoImpl implements ItemDao {
                     rs.getString("item_name"),
                     rs.getInt("item_price"),
                     rs.getInt("item_increase"),
-                    rs.getInt("item_type") );
+                    rs.getString("item_explanation"),
+                    rs.getString("item_type") );
                 list.add(itemdto);
             }
         }finally {
