1f01e3aaa85f36d9a6b42929f818b1baf2578f68
imnotpossib1e <habit2me@gmail.com>
2026-09-10 15:31:24 +0900

    상점 나가기 로직

--- src/com/callitaday/monsterhunter/view/MenuView.java
+++ src/com/callitaday/monsterhunter/view/MenuView.java
@@ -126,17 +126,48 @@ public class MenuView {
      * 상점 메뉴
      */
     public static void shopView(int userId){
-        // 판매 목록 띄우기
+//        // 판매 목록 띄우기
+//        ItemController.selectAllItem();
+//
+//        // 내가 보유한 아이템 띄우기
+//
+//        // 구매할 아이템, 수량 받기
+//        System.out.println("메인 메뉴로 돌아가기 : Q");
+//        System.out.print("구매할 아이템 번호 > ");
+//        int item_id = Integer.parseInt(sc.nextLine());
+//        System.out.print("구매할 아이템 수량 > ");
+//        int quantity = Integer.parseInt(sc.nextLine());
+//        ShopController.purchaceItem(userId, item_id, quantity);
+
         ItemController.selectAllItem();
+        boolean validInput = true;
+        // 선택지가 유효할 때 까지 반복
+        while(validInput){
+            // 구매할 아이템, 수량 받기
+            System.out.println("메인 메뉴로 돌아가기 : Q");
+            System.out.print("구매할 아이템 번호 > ");
+            String input = sc.nextLine();
 
-        // 내가 보유한 아이템 띄우기
+            if (input.equalsIgnoreCase("Q")) {
+                System.out.println("메인 메뉴로 돌아갑니다.");
+                break;
+            }
 
-        // 구매할 아이템, 수량 받기
-        System.out.print("구매할 아이템 번호 > ");
-        int item_id = Integer.parseInt(sc.nextLine());
-        System.out.print("구매할 아이템 수량 > ");
-        int quantity = Integer.parseInt(sc.nextLine());
-        ShopController.purchaceItem(userId, item_id, quantity);
+            try {
+                int item_id = Integer.parseInt(input);
+                System.out.print("구매할 아이템 수량 > ");
+                int quantity = Integer.parseInt(sc.nextLine());
+                ShopController.purchaceItem(userId, item_id, quantity);
+            } catch (NumberFormatException e) {
+                System.out.println("올바른 번호를 입력해주세요.");
+            }
+        }
+    }
+
+    /**
+     * 아이템 구매 메뉴
+     */
+    public static void shopChoiceView(int userId){
 
     }
 
