31677a449490d58dbc0320074cd4256ea7079aaa
imnotpossib1e <habit2me@gmail.com>
2026-09-13 02:16:40 +0900

    코드 삭제

--- src/com/callitaday/monsterhunter/service/ShopService.java
+++ src/com/callitaday/monsterhunter/service/ShopService.java
@@ -1,9 +1,12 @@
 package com.callitaday.monsterhunter.service;
 
+import com.callitaday.monsterhunter.dto.CharacterInfoDto;
+import com.callitaday.monsterhunter.dto.InventoryDto;
 import com.callitaday.monsterhunter.exception.AddException;
 import com.callitaday.monsterhunter.exception.ModifyException;
 import com.callitaday.monsterhunter.exception.NotFoundException;
 import com.callitaday.monsterhunter.exception.PurchaseFailException;
+import java.sql.SQLException;
 
 public interface ShopService {
 
@@ -12,4 +15,8 @@ public interface ShopService {
      */
     public void purchaseItem(int user_id, int item_id, int quantity) throws AddException, ModifyException, PurchaseFailException, NotFoundException;
 
+    /**
+     * 상점 구매 유효성 검사
+     */
+    public void validatePurchase (CharacterInfoDto characterInfoDto, InventoryDto inventoryDto, int itemId, int quantity) throws PurchaseFailException, SQLException;
 }
