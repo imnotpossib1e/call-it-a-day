1f065ab44f765ef91952bccf4c0489314a1f875d
sdh02142 <sdh02142@naver.com>
2026-09-13 21:42:20 +0900

    방어하기 확률 조정(난이도 하향)

--- src/com/callitaday/monsterhunter/service/BattleServiceImpl.java
+++ src/com/callitaday/monsterhunter/service/BattleServiceImpl.java
@@ -199,9 +199,9 @@ public class BattleServiceImpl implements BattleService{
 		boolean result = false;
 
 		// 적의 데미지 계산
-	    if (userDice >= (enemyDice*0.7)) {
+	    if (userDice >= enemyDice) {
 			// 나의 방어가 성공해서 공격 데미지가 반사되는 경우
-	    	if(userDice == 10) {
+	    	if(userDice >= 7) {
 	    		int counterDamage = (userDice - enemyDice) * 10;
 
 				enemy.setEnemyHp(Math.max(0, enemy.getEnemyHp() - counterDamage));
